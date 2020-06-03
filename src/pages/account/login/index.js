ecb5719556f63b726f761538ec828d7308518eda
uksir <yeah126139163@163.com>
2020-06-03 21:40:40 +0800

    项目搭建

---
+++ src/pages/account/login/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+class Index extends Component {
+  render() {
+    return (
+      <View><Text>登录</Text></View>
+    );
+  }
+}
+export default Index;
