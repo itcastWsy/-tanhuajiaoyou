b3a5206af705ad5720b0c390c27600807fd7f67b
uksir <yeah126139163@163.com>
2020-06-04 12:24:18 +0800

    验证手机号码

--- src/pages/account/login/index.js
+++ src/pages/account/login/index.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import { View, Text, Image, StatusBar } from 'react-native';
 import { pxToDp } from "../../../utils/stylesKits";
-import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
+import validator from "../../../utils/validator";
 class Index extends Component {
+  state = {
+    // 手机号码
+    phoneNumber: "15915912345",
+    // 手机号码是否合法
+    phoneValid: true
+  }
+  // 登录框手机号码输入
+  phoneNumberChangeText = (phoneNumber) => {
+    this.setState({ phoneNumber });
+    console.log(phoneNumber);
+  }
+  // 手机号码点击 完成
+  phoneNumberSubmitEditing = () => {
+    /* 
+    1 对手机号码的合法性做校验 -  正则
+       1 不通过  提示
+    2 将手机号码发送到后台对应接口 -> 获取验证码
+    3 将登录页面切换成 填写验证码的界面 
+     */
+    //  validator.validatePhone
+    const { phoneNumber } = this.state;
+    const phoneValid = validator.validatePhone(phoneNumber);
+    if (!phoneValid) {
+      // 没有通过
+      this.setState({ phoneValid });
+      return;
+    }
+
+  }
   render() {
+    const { phoneNumber, phoneValid } = this.state;
     return (
       <View>
         {/* 0.0  状态栏 开始 */}
@@ -16,20 +46,25 @@ class Index extends Component {
         {/* 1.0 背景图片 结束*/}
 
         {/* 2.0 内容 开始 */}
-        <View style={{padding:pxToDp(20)}}>
+        <View style={{ padding: pxToDp(20) }}>
           {/* 2.1 登录 开始 */}
           <View>
             {/* 标题 */}
-            <View><Text style={{fontSize:pxToDp(25),color:"#888",fontWeight:"bold"}}>手机号登录注册</Text></View>
+            <View><Text style={{ fontSize: pxToDp(25), color: "#888", fontWeight: "bold" }}>手机号登录注册</Text></View>
             {/* 输入框 */}
-            <Input
-  placeholder='BASIC INPUT'
-/>
-
-<Input
-  placeholder='INPUT WITH ICON'
-  leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
-/>
+            <View style={{ marginTop: pxToDp(30) }}> 
+              <Input
+                placeholder='请输入手机号码'
+                maxLength={11}
+                keyboardType="phone-pad"
+                value={phoneNumber}
+                inputStyle={{ color: "#333" }}
+                onChangeText={this.phoneNumberChangeText}
+                errorMessage={phoneValid ? "" : "手机号码格式不正确"}
+                onSubmitEditing={this.phoneNumberSubmitEditing}
+                leftIcon={{ type: 'font-awesome', name: 'phone', color: "#ccc", size: pxToDp(20) }}
+              />
+            </View>
           </View>
           {/* 2.1 登录 结束 */}
         </View>
