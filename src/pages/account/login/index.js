acd67606cc4204ff742806dc93af92599c00d186
uksir <yeah126139163@163.com>
2020-06-04 10:11:10 +0800

    登录页面-背景图片

--- src/pages/account/login/index.js
+++ src/pages/account/login/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,Image ,StatusBar} from 'react-native';
 class Index extends Component {
   render() {
-    return (
-      <View><Text>登录</Text></View>
+    return ( 
+      <View>
+        {/* 0.0  状态栏 开始 */}
+        <StatusBar backgroundColor="transparent" translucent={true}  />
+        {/* 0.0  状态栏 结束 */}
+        {/* 1.0 背景图片 开始 */}
+        {/* 200 单位 dp 单位px -> dp单位? */}
+        <Image style={{width:"100%",height:200}} source={require("../../../res/profileBackground.jpg")} />
+        {/* 1.0 背景图片 结束*/}
+      </View>
     );
   }
 }
