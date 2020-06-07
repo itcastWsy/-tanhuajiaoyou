a5c2ad6d289cf99d0e4cdfc1c77fe28c08161536
uksir <yeah126139163@163.com>
2020-06-07 16:31:32 +0800

    完成填写资料页面的标题

--- src/pages/account/userinfo/index.js
+++ src/pages/account/userinfo/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { pxToDp } from "../../../utils/stylesKits";
 class Index extends Component {
   render() {
     return (
-      <View><Text>完善个人信息页面</Text></View>
+      <View style={{ backgroundColor: "#fff", flex: 1, padding: pxToDp(20) }}>
+        {/* 1.0 标题 开始 */}
+        <Text style={{fontSize:pxToDp(20),color:"#666",fontWeight:"bold"}} >填写资料</Text>
+        <Text style={{fontSize:pxToDp(20),color:"#666",fontWeight:"bold"}} >提升我的魅力</Text>
+        {/* 1.0 标题 结束 */}
+      </View>
     );
   }
 }
