c4a42a433a603f692756114a919c5a4ffcc949ab
uksir <yeah126139163@163.com>
2020-06-08 21:07:57 +0800

    上传头像

--- src/mobx/index.js
+++ src/mobx/index.js
@@ -2,11 +2,11 @@ import { observable, action } from "mobx";
 
 class RootStore {
   // 手机号码
-  @observable mobile = "";
+  @observable mobile = "15915912346";
   // token
-  @observable token = "";
+  @observable token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjgsIm5hbWUiOiIxNTkxNTkxMjM0NiIsImlhdCI6MTU5MTYxODcwOCwiZXhwIjoxNjE3NTM4NzA4fQ._WMeRhIU2XOZe1qG1u106tw2Jz57WuWWLLsPVe1oAAA";
   // 用户的唯一id 
-  @observable userId = "";
+  @observable userId = "159159123461591515495983";
 
   @action setUserInfo(mobile,token,userId) {
     this.mobile = mobile;
