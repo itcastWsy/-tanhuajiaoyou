ace1717d415595963a1b36f0308d64681dbb8de8
uksir <yeah126139163@163.com>
2020-06-10 18:04:42 +0800

    交友-首页-顶部图片吸附效果

--- src/pages/friend/home/index.js
+++ src/pages/friend/home/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
- import { View, Text } from 'react-native';
- class Index extends Component {
- render() {
- return (
- <View><Text>交友-首页</Text></View>
- );
- }
- }
+import { View, Text,StatusBar } from 'react-native';
+import HeaderImageScrollView from 'react-native-image-header-scroll-view';
+import { pxToDp } from '../../../utils/stylesKits';
+
+
+class Index extends Component {
+  render() {
+    return (
+      <HeaderImageScrollView
+        maxHeight={pxToDp(130)}
+        minHeight={pxToDp(44)}
+        headerImage={require("../../../res/headfriend.png")}
+        renderForeground={() => (
+          <View style={{ height: pxToDp(130), justifyContent: "center", alignItems: "center" }} >
+           <StatusBar backgroundColor={"transparent"} translucent={true}  />
+          </View>
+        )}
+      >
+        <View style={{ height: 1000 }}>
+         
+        </View>
+      </HeaderImageScrollView>
+    );
+  }
+}
 export default Index;
