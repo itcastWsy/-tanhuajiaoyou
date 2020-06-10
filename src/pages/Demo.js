d9226553c10f67b5906007cdaca7356f9289af0f
uksir <yeah126139163@163.com>
2020-06-10 10:43:22 +0800

    引入极光通讯

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,58 +1,33 @@
-import React, {Component} from 'react';
-import {Text, StyleSheet} from 'react-native';
- 
-import {
-  CodeField,
-  Cursor
-} from 'react-native-confirmation-code-field';
- 
-const styles = StyleSheet.create({
-  root: {flex: 1, padding: 20},
-  title: {textAlign: 'center', fontSize: 30},
-  codeFiledRoot: {marginTop: 20},
-  cell: {
-    width: 40,
-    height: 40,
-    lineHeight: 38,
-    fontSize: 24,
-    borderBottomWidth: 2,
-    borderColor: '#00000030',
-    textAlign: 'center',
-    color:"#7d53ea"
-  },
-  focusCell: {
-    borderColor: '#7d53ea'
-  },
-});
- 
- 
-class App extends Component {
-  state = { 
-    vcodeTxt:""
-   }
+import React from 'react';
+import { View, Text } from "react-native";
+import JMessage from "jmessage-react-plugin";
+class App extends React.Component {
+  componentDidMount() {
+    JMessage.init({
+      'appkey': 'c0c08d3d8babc318fe25bb0c',
+      'isOpenMessageRoaming': true,
+      'isProduction': false,
+      'channel': '' 
+    })
 
-   onVcodeChangeText=(vcodeTxt)=>{
-     this.setState({vcodeTxt   });
-   }
-  render() { 
+    JMessage.login({
+      username: "18665711956",
+      password: "18665711956"
+    }, (res) => {
+      console.log("登录成功");
+      console.log(res);
+    }, (err) => {
+      console.log("登录失败");
+      console.log(err);
+    })
+
+  }
+  render() {
     return (
-        <CodeField
-          value={this.state.vcodeTxt}
-          onChangeText={this.onVcodeChangeText}
-          cellCount={6}
-          rootStyle={styles.codeFiledRoot}
-          keyboardType="number-pad"
-          renderCell={({index, symbol, isFocused}) => (
-            <Text
-              key={index}
-              style={[styles.cell, isFocused && styles.focusCell]}
-              >
-              {symbol || (isFocused ? <Cursor /> : null)}
-            </Text>
-          )}
-        />
+      <View>
+        <Text>goods</Text>
+      </View>
     );
   }
 }
- 
 export default App;
