26abf008b0926c4f3344161a9ad4802a7c0efa69
uksir <yeah126139163@163.com>
2020-06-10 14:17:51 +0800

    优化极光通讯的代码

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import { View, Text } from "react-native";
-import JMessage from "jmessage-react-plugin";
+import JMessage from "../utils/JMessage";
 class App extends React.Component {
   componentDidMount() {
-    JMessage.init({
-      'appkey': 'c0c08d3d8babc318fe25bb0c',
-      'isOpenMessageRoaming': true,
-      'isProduction': false,
-      'channel': '' 
-    })
-
-    JMessage.login({
-      username: "18665711956",
-      password: "18665711956"
-    }, (res) => {
-      console.log("登录成功");
-      console.log(res);
-    }, (err) => {
-      console.log("登录失败");
-      console.log(err);
-    })
+    JMessage.init();
+    JMessage.login("sfdsfdf","dfdsfdf")
+    .then(console.log);
 
   }
   render() {
