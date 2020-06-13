f313d57d43a605d6c95f2ce796026690e174d413
uksir <yeah126139163@163.com>
2020-06-13 09:24:26 +0800

    获取推荐朋友数据

--- src/pages/friend/home/index.js
+++ src/pages/friend/home/index.js
@@ -5,7 +5,43 @@ import { pxToDp } from '../../../utils/stylesKits';
 import FriendHead from "./components/FriendHead";
 import Visitors from "./components/Visitors";
 import PerfectGirl from "./components/PerfectGirl";
+import request from "../../../utils/request";
+import { FRIENDS_RECOMMEND } from "../../../utils/pathMap";
+// id: 7
+// header: "/upload/18665711978.png"
+// nick_name: "一叶知秋"
+// gender: "男"
+// age: 23
+// marry: "单身"
+// xueli: "本科"
+// dist: 0
+// agediff: 0
+// fateValue: 40
 class Index extends Component {
+  state={
+    // 接口要的数据
+    params:{
+      page:1,
+      pagesize:10,
+      gender:"男",
+      distance:2,
+      lastLogin:"",
+      city:"",
+      education:""
+    },
+    // 推荐朋友 数组
+    recommends:[]
+  }
+
+  componentDidMount() {
+    this.getRecommends();
+  }
+
+  // 获取推荐朋友
+  getRecommends=async ()=>{
+    const res=await request.privateGet(FRIENDS_RECOMMEND,this.state.params);
+    this.setState({ recommends: res.data });
+  }
   render() {
     return (
       <HeaderImageScrollView
