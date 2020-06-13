1bd2514bff8094945537f87f592b5d3ff856aac4
uksir <yeah126139163@163.com>
2020-06-13 16:55:39 +0800

    加载数据动态渲染滑动图片

--- src/pages/friend/tanhua/index.js
+++ src/pages/friend/tanhua/index.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground,StyleSheet,Button } from 'react-native';
+import { View, Text, ImageBackground,StyleSheet,Image } from 'react-native';
 import THNav from "../../../components/THNav";
 import Swiper from "react-native-deck-swiper";
+import request from "../../../utils/request";
+import { FRIENDS_CARDS, BASE_URI } from "../../../utils/pathMap";
 class Index extends Component {
+  params={
+    page:1,
+    pagesize:5
+  }
+  state={
+    cards:[
+//       id: 8
+// header: "/upload/13828459782.png"
+// nick_name: "雾霭朦胧"
+// age: 21
+// gender: "女"
+// marry: "未婚"
+// xueli: "大专"
+// dist: 0
+    ]
+  }
+
+  componentDidMount() {
+    this.getFriendsCards();
+  }
+  
+  // 获取要渲染的数据
+  getFriendsCards=async ()=>{
+    const res=await request.privateGet(FRIENDS_CARDS,this.params);
+    this.setState({ cards :res.data  });
+
+  }
   render() {
+    const {cards}=this.state;
+    if(cards.length===0){
+      return <></>
+    }
     return (
       <View style={{ flex: 1, backgroundColor: "#fff" }}>
         <THNav title="探花" />
@@ -12,44 +45,36 @@ class Index extends Component {
           imageStyle={{ height: "100%" }}
           source={require("../../../res/testsoul_bg.png")}
         >
-          <View style={styles.container}>
             <Swiper
-              cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+              cards={cards}
               renderCard={(card) => {
                 return (
-                  <View style={styles.card}>
-                    <Text style={styles.text}>{card}</Text>
+                  <View style={styles.card} >
+                  <Image source={{uri:BASE_URI+card.header}} 
+                  style={{width:"100%",height:"80%"}}
+                  />
                   </View>
                 )
               }}
               onSwiped={(cardIndex) => { console.log(cardIndex) }}
               onSwipedAll={() => { console.log('onSwipedAll') }}
               cardIndex={0}
-              backgroundColor={'#4FD0E9'}
+              backgroundColor={'transparent'}
+              cardVerticalMargin={0}
               stackSize={6}>
-              <Button
-                onPress={() => { console.log('oulala') }}
-                title="Press me">
-                You can press me
-            </Button>
             </Swiper>
-          </View>
         </ImageBackground>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5FCFF"
-  },
+
   card: {
-    flex: 1,
+   
     borderRadius: 4,
     borderWidth: 2,
     borderColor: "#E8E8E8",
-    justifyContent: "center",
     backgroundColor: "white"
   },
   text: {
