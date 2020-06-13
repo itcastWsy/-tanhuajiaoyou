36352f59de3e75ccf030da97e75dbde43a836ab4
uksir <yeah126139163@163.com>
2020-06-13 11:38:22 +0800

    完成了选择近期登录时间的功能

--- src/pages/friend/home/components/FilterPanel.js
+++ src/pages/friend/home/components/FilterPanel.js
@@ -4,6 +4,7 @@ import IconFont from "../../../../components/IconFont";
 import { pxToDp } from '../../../../utils/stylesKits';
 import SvgUri from "react-native-svg-uri";
 import {male,female  } from "../../../../res/fonts/iconSvg";
+import Picker from 'react-native-picker';
 class Index extends Component {
   // gender: "男",
   // distance: 2,
@@ -19,8 +20,28 @@ class Index extends Component {
   chooeseGender=(gender)=>{
     this.setState({ gender  });
   }
+
+  // 选择近期登录时间
+  chooeseLastLogin=()=>{
+    Picker.init({
+      pickerData: [ "15分钟","1天","1小时","不限制"],
+      selectedValue: [this.state.lastLogin],
+      wheelFlex: [1, 0, 0], // 显示省和市
+      pickerConfirmBtnText: "确定",
+      pickerCancelBtnText: "取消",
+      pickerTitleText: "选择近期登录时间",
+      onPickerConfirm: data => {
+        this.setState(
+          {
+            lastLogin: data[0]
+          }
+        );
+      }
+    });
+    Picker.show();
+  }
   render() {
-    const { gender } = this.state;
+    const { gender,lastLogin } = this.state;
     return (
       <View
         style={{
@@ -63,6 +84,12 @@ class Index extends Component {
           {/* 性别 图标 结束 */}
         </View>
         {/* 2.0 性别 结束 */}
+        {/* 3.0 近期登录时间 开始 */}
+        <View style={{flexDirection:"row",alignItems:"center",marginTop:pxToDp(10)}}>
+          <Text style={{ color: "#777", fontSize: pxToDp(18), width: pxToDp(140) }}>近期登录时间:</Text>
+          <Text onPress={this.chooeseLastLogin}  style={{ color: "#777", fontSize: pxToDp(18)}}>{lastLogin||"请选择"}</Text>
+        </View>
+        {/* 3.0 近期登录时间 结束 */}
       </View>
     );
   }
