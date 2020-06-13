b7e2177cddf88e6bed94c79d20f866199ff09a8c
uksir <yeah126139163@163.com>
2020-06-13 12:11:44 +0800

    完成了选择距离功能

--- src/pages/friend/home/components/FilterPanel.js
+++ src/pages/friend/home/components/FilterPanel.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text ,TouchableOpacity} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import IconFont from "../../../../components/IconFont";
 import { pxToDp } from '../../../../utils/stylesKits';
 import SvgUri from "react-native-svg-uri";
-import {male,female  } from "../../../../res/fonts/iconSvg";
+import { male, female } from "../../../../res/fonts/iconSvg";
 import Picker from 'react-native-picker';
+import {Slider  } from "react-native-elements";
 class Index extends Component {
   // gender: "男",
   // distance: 2,
@@ -17,14 +18,14 @@ class Index extends Component {
   }
 
   // 选择性别
-  chooeseGender=(gender)=>{
-    this.setState({ gender  });
+  chooeseGender = (gender) => {
+    this.setState({ gender });
   }
 
   // 选择近期登录时间
-  chooeseLastLogin=()=>{
+  chooeseLastLogin = () => {
     Picker.init({
-      pickerData: [ "15分钟","1天","1小时","不限制"],
+      pickerData: ["15分钟", "1天", "1小时", "不限制"],
       selectedValue: [this.state.lastLogin],
       wheelFlex: [1, 0, 0], // 显示省和市
       pickerConfirmBtnText: "确定",
@@ -41,7 +42,7 @@ class Index extends Component {
     Picker.show();
   }
   render() {
-    const { gender,lastLogin } = this.state;
+    const { gender, lastLogin, distance } = this.state;
     return (
       <View
         style={{
@@ -62,7 +63,7 @@ class Index extends Component {
         </View>
         {/* 1.0 标题 结束 */}
         {/* 2.0 性别 开始 */}
-        <View style={{flexDirection:"row",alignItems:"center",marginTop:pxToDp(10)}}>
+        <View style={{ flexDirection: "row", alignItems: "center", marginTop: pxToDp(10) }}>
           <Text style={{ color: "#777", fontSize: pxToDp(18), width: pxToDp(80) }} >性别:</Text>
           {/* 性别 图标 开始 */}
           <View style={{ justifyContent: "space-around", width: "50%", flexDirection: "row", alignSelf: "center" }}>
@@ -85,11 +86,23 @@ class Index extends Component {
         </View>
         {/* 2.0 性别 结束 */}
         {/* 3.0 近期登录时间 开始 */}
-        <View style={{flexDirection:"row",alignItems:"center",marginTop:pxToDp(10)}}>
+        <View style={{ flexDirection: "row", alignItems: "center", marginTop: pxToDp(10) }}>
           <Text style={{ color: "#777", fontSize: pxToDp(18), width: pxToDp(140) }}>近期登录时间:</Text>
-          <Text onPress={this.chooeseLastLogin}  style={{ color: "#777", fontSize: pxToDp(18)}}>{lastLogin||"请选择"}</Text>
+          <Text onPress={this.chooeseLastLogin} style={{ color: "#777", fontSize: pxToDp(18) }}>{lastLogin || "请选择"}</Text>
         </View>
         {/* 3.0 近期登录时间 结束 */}
+        {/* 4.0 距离 开始 */}
+        <View style={{ marginTop: pxToDp(10) }}>
+          <Text style={{ color: "#777", fontSize: pxToDp(18)}}>距离:{distance || 0} KM</Text>
+            <Slider
+            value={distance}
+            minimumValue={0}
+            maximumValue={10}
+            step={0.5}
+            onValueChange={(distance)=>this.setState({ distance })}
+            />
+        </View>
+        {/* 4.0 距离 结束 */}
       </View>
     );
   }
