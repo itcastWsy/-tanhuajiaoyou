531ffab7d2646edc94420ed11222b946ed337444
uksir <yeah126139163@163.com>
2020-06-14 17:58:13 +0800

    点击弹出筛选组件

--- src/pages/friend/search/index.js
+++ src/pages/friend/search/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text,Image, StatusBar, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StatusBar, ImageBackground, TouchableOpacity } from 'react-native';
 import request from "../../../utils/request";
-import { FRIENDS_SEARCH,BASE_URI } from "../../../utils/pathMap";
+import { FRIENDS_SEARCH, BASE_URI } from "../../../utils/pathMap";
 import { pxToDp, screenWidth, screenHeight } from "../../../utils/stylesKits";
+import IconFont from "../../../components/IconFont";
+import {Overlay  } from "teaset";
+import FilterPanel from "./components/FilterPanel";
 class Index extends Component {
   params = {
     gender: "男",
@@ -55,6 +58,25 @@ class Index extends Component {
     console.log(res);
     this.setState({ list: res.data });
   }
+
+  // 点击筛选按钮
+  handleFilterShow=()=>{
+    
+    let overlayViewRef=null;
+    let overlayView = (
+      <Overlay.View
+        modal={true}
+        overlayOpacity={0.3}
+        ref={v => overlayViewRef = v}
+        >
+          {/* 显示 筛选组件 */}
+          <FilterPanel onSubmitFilter={this.handleSubmitFilter}  params={this.params} onClose={()=>overlayViewRef.close()} />
+      </Overlay.View>
+    );
+    Overlay.show(overlayView);
+  }
+  // 获取了筛选结果
+  handleSubmitFilter=(filterParams)=>{}
   render() {
     const { list } = this.state;
     return (
@@ -63,27 +85,37 @@ class Index extends Component {
         source={require("../../../res/search.gif")}
       >
         <StatusBar backgroundColor={"transparent"} translucent={true} />
+        <TouchableOpacity
+        style={{backgroundColor:"#fff",position:"absolute",right:"10%",top:"10%",
+        width:pxToDp(55),height:pxToDp(55),borderRadius:pxToDp(27.5),alignItems:"center",justifyContent:"center",
+        zIndex:1000
+        }}
+        onPress={this.handleFilterShow}
+        >
+          <IconFont style={{color:"#912375",fontSize:pxToDp(30)}}  name="iconshaixuan" />
+        </TouchableOpacity>
         {
           list.map((v, i) => {
             const whMap = this.WHMap[this.getWidthHeight(v.dist)];
-            const tx=Math.random()*(screenWidth-whMap.width);
-            const ty=Math.random()*(screenHeight-whMap.height);
+            const tx = Math.random() * (screenWidth - whMap.width);
+            const ty = Math.random() * (screenHeight - whMap.height);
             return <TouchableOpacity
               key={i}
-              style={{position:"absolute",left:tx,top:ty}}  
+              style={{ position: "absolute", left: tx, top: ty }}
             >
               <ImageBackground
                 source={require("../../../res/showfirend.png")}
                 resizeMode="stretch"
-                style={{ ...whMap,position:"relative",alignItems:"center" }}
+                style={{ ...whMap, position: "relative", alignItems: "center" }}
               >
                 <Text
-                numberOfLines={1}
-                style={{color:"#ffffff9a",position:"absolute",top:-pxToDp(20)}}
+                  numberOfLines={1}
+                  style={{ color: "#ffffff9a", position: "absolute", top: -pxToDp(20) }}
                 >{v.nick_name}</Text>
-                <Image style={{width:whMap.width,height:whMap.width,
-                borderRadius:whMap.width/2
-                }} source={{uri:BASE_URI+v.header}}  />
+                <Image style={{
+                  width: whMap.width, height: whMap.width,
+                  borderRadius: whMap.width / 2
+                }} source={{ uri: BASE_URI + v.header }} />
               </ImageBackground>
 
             </TouchableOpacity>
