0c8f25e0875ecdad9ddacc133d0c000ff9916059
uksir <yeah126139163@163.com>
2020-06-15 10:14:27 +0800

    搭建页面基本结构

--- src/pages/friend/testSoul/testQA/index.js
+++ src/pages/friend/testSoul/testQA/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 import request from "../../../../utils/request";
-import {FRIENDS_QUESTIONSECTION} from "../../../../utils/pathMap";
-
+import { FRIENDS_QUESTIONSECTION } from "../../../../utils/pathMap";
+import THNav from "../../../../components/THNav";
+import { pxToDp } from '../../../../utils/stylesKits';
 // qid: 1
 // type: "初级"
 // title: "初级灵魂题"
@@ -14,9 +15,14 @@ import {FRIENDS_QUESTIONSECTION} from "../../../../utils/pathMap";
 // istested: true
 // islock: false
 class Index extends Component {
-  state={
+  titles={
+    "初级":require("../../../../res/leve1.png"),
+    "中级":require("../../../../res/leve2.png"),
+    "高级":require("../../../../res/leve3.png")
+  }
+  state = {
     // 测试题问卷列表数据
-    questionList:[
+    questionList: [
       // {
       //   "qsid": 1,
       //   "question_title": "未来生活的幸福指数，跟物质和精神哪个关系更 大？",
@@ -34,21 +40,49 @@ class Index extends Component {
       //   ]
       // }
     ],
-    currentIndex:0
+    currentIndex: 0
   }
   componentDidMount() {
     this.getList();
   }
   // 获取测试题问卷
-  getList=async()=>{
-    const url=FRIENDS_QUESTIONSECTION.replace(":id",this.props.route.params.qid);
-    const res=await request.privateGet(url);
-    this.setState({ questionList:res.data  });
+  getList = async () => {
+    const url = FRIENDS_QUESTIONSECTION.replace(":id", this.props.route.params.qid);
+    const res = await request.privateGet(url);
+    this.setState({ questionList: res.data });
   }
   render() {
     console.log(this.props.route.params);
+    const question = this.props.route.params;
     return (
-      <View><Text>填写问卷页面</Text></View>
+      <View style={{ flex: 1, backgroundColor: "#fff" }}>
+        <THNav title={question.title} />
+        <ImageBackground
+          source={require("../../../../res/qabg.png")}
+          style={{ width: "100%", height: "100%" }}
+        >
+
+          {/* 1.0 两侧图标 开始 */}
+          <View style={{ marginTop: pxToDp(60), flexDirection: 'row', justifyContent: "space-between" }}>
+            <ImageBackground
+              style={{
+                width: pxToDp(66), height: pxToDp(52), justifyContent: 'center', alignItems: "flex-end"
+              }}
+              source={require("../../../../res/qatext.png")}
+            >
+            </ImageBackground>
+            <ImageBackground
+              style={{
+                width: pxToDp(66), height: pxToDp(52), justifyContent: 'center', alignItems: "flex-end"
+              }}
+              source={this.titles[question.type]}
+            >
+            </ImageBackground>
+          </View>
+          {/* 1.0 两侧图标 结束 */}
+
+        </ImageBackground>
+      </View>
     );
   }
 }
