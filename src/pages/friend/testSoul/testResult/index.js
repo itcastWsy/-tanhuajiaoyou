0a42c4bb0fdf02119e44078a194b7f6d178a8f48
uksir <yeah126139163@163.com>
2020-06-15 12:34:16 +0800

    完成结果页面-背景和标题

--- src/pages/friend/testSoul/testResult/index.js
+++ src/pages/friend/testSoul/testResult/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+import THNav from "../../../../components/THNav";
+import { pxToDp } from '../../../../utils/stylesKits';
 class Index extends Component {
   render() {
     console.log(this.props.route.params);
+    const params=this.props.route.params;
+
     return (
-      <View><Text>测试结果</Text></View>
+      <ImageBackground
+        style={{ flex: 1, width: "100%" }}
+        source={require("../../../../res/qabg.png")}
+      >
+        <THNav title="测试结果" />
+        <ImageBackground
+          style={{ flex: 1, width: "100%" ,position:'relative'}}
+          resizeMode="stretch"
+          source={require("../../../../res/result.png")}
+        >
+
+          <Text style={{
+            position:'absolute',top:"1%",left:"6%",color:"#ffffff9a",letterSpacing:pxToDp(7)
+          }}>灵魂基因鉴定单</Text>
+
+          {/* 用户的名称 */}
+          <View style={{flexDirection:"row",justifyContent:"space-between",
+          width:"47%",position:"absolute",top:"6%",right:"5%"
+        }}>
+            <Text style={{color:"#fff",fontSize:pxToDp(16)}} >[</Text>
+            <Text style={{color:"#fff",fontSize:pxToDp(16)}} >{params.currentUser.nick_name}</Text>
+            <Text style={{color:"#fff",fontSize:pxToDp(16)}} >]</Text>
+          </View>
+        </ImageBackground>
+      </ImageBackground>
     );
   }
 }
