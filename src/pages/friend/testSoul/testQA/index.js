f5d3500de67cd12dd08294fd248184d8f218a994
uksir <yeah126139163@163.com>
2020-06-15 11:28:20 +0800

    完成测试-提交答案

--- src/pages/friend/testSoul/testQA/index.js
+++ src/pages/friend/testSoul/testQA/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text, ImageBackground, Image } from 'react-native';
 import request from "../../../../utils/request";
-import { FRIENDS_QUESTIONSECTION, BASE_URI } from "../../../../utils/pathMap";
+import { FRIENDS_QUESTIONSECTION, BASE_URI ,FRIENDS_QUESTIONANS} from "../../../../utils/pathMap";
 import THNav from "../../../../components/THNav";
 import { pxToDp } from '../../../../utils/stylesKits';
 import { inject, observer } from 'mobx-react';
@@ -25,6 +25,7 @@ class Index extends Component {
     "中级": require("../../../../res/leve2.png"),
     "高级": require("../../../../res/leve3.png")
   }
+  ansList=[];
   state = {
     // 测试题问卷列表数据
     questionList: [
@@ -76,6 +77,50 @@ class Index extends Component {
         break;
     }
     return numCn;
+  }
+  // 选择了答案
+  chooeseAns=async (ans)=>{
+    const {currentIndex,questionList}=this.state;
+    this.ansList.push(ans);
+    if(currentIndex>=questionList.length-1){
+      // 最后一题了 
+      const url=FRIENDS_QUESTIONANS.replace(":id",this.props.route.params.qid);
+      const answers=this.ansList.join(",");
+      const res=await request.privatePost(url,{answers});
+      console.log(res);
+      
+    // "abstract": 80,
+    // "content": "感性动物，更加在乎恋爱中双方的感受。 对生活有着相同的感触，也许经历不同，但你们都知道物质才是生活的基础，没有面包的爱情也许不会幸福； 爱情中，希望得到彼此更多的爱。 也许你都经历过生活的坎坷，所以你们都希望有一个安安稳稳的未来生活，两个孩子一只狗，一个温馨的家是你们的归属。 希望在细节上给与对方最大的关怀，从生活的点点滴滴去珍爱对方。 属于爱情中的”奉献者“，期望对方比自己更快乐。 对生活有着更长远的打算，也许当下苦一点，并不会对你们造成多少伤害，你们期许的是一个更好的未来。 在日常生活中，都知道自己想要什么不想要什么，所以很少会冲动消费。 对命运这种事情，有着难得一致的认同感，都认为有些感情早已安排妥当，只是静静的等待我们去发现。 对人对事对物，都可以极大程度的包容，这使你们未来沟通起来会更加顺畅。",
+    // "currentUser": {
+    //   "Distance": 0,
+    //   "address": "广州市天河区珠吉路58号",
+    //   "age": 23,
+    //   "amount": null,
+    //   "birthday": "1995-03-07T16: 00: 00.000Z",
+    //   "city": "广州",
+    //   "email": null,
+    //   "gender": "男",
+    //   "guid": null,
+    //   "header": "/upload/18665711978.png",
+    //   "id": 7,
+    //   "lat": 23.12933,
+    //   "lng": 113.42782,
+    //   "login_time": "2020-06-10T10: 57: 34.000Z",
+    //   "marry": "单身",
+    //   "mobile": "18665711978",
+    //   "nick_name": "一叶知秋",
+    //   "status": 0,
+    //   "vcode": "888888",
+    //   "xueli": "本科"
+    // },
+    // "extroversion": 75,
+    // "judgment": 90,
+    // "qid": 1,
+    // "rational": 88
+  
+    }else{
+      this.setState({ currentIndex: currentIndex+1 });
+    }
   }
   render() {
     const { currentIndex, questionList } = this.state;
@@ -131,6 +176,7 @@ class Index extends Component {
 
             <View style={{ width: "100%" }}>
               {questionList[currentIndex].answers.map((v, i) => <TouchableOpacity 
+              onPress={this.chooeseAns.bind(this,v.ans_No)}
               key={i}
               style={{ marginTop: pxToDp(10) }}>
                 <LinearGradient
