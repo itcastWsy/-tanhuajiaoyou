032ac39aaedb4466e42b47db940310d16e64a96e
uksir <yeah126139163@163.com>
2020-06-16 15:41:38 +0800

    分析接口获取数据

--- src/pages/friend/detail/index.js
+++ src/pages/friend/detail/index.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import request from "../../../utils/request";
+import { FRIENDS_PERSONALINFO } from "../../../utils/pathMap";
 class Index extends Component {
+  state={
+    userDetail:{}
+  }
+  params={
+    page:1,
+    pagesize:5
+  }
+
+  componentDidMount() {
+    this.getDetail();  
+  }
+
+  // 获取朋友详情
+  getDetail=async()=>{
+    const url=FRIENDS_PERSONALINFO.replace(":id",this.props.route.params.id);
+    const res=await request.privateGet(url,this.params);
+    this.setState({ userDetail:res.data  });
+  }
   render() {
     console.log(this.props.route.params);
     return (
