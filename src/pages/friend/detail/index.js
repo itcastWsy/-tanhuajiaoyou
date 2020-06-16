bd27ee52c7f6314d0b5d94c91c9e5ff422528b15
uksir <yeah126139163@163.com>
2020-06-16 16:03:12 +0800

    显示轮播图

--- src/pages/friend/detail/index.js
+++ src/pages/friend/detail/index.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import request from "../../../utils/request";
-import { FRIENDS_PERSONALINFO } from "../../../utils/pathMap";
+import { FRIENDS_PERSONALINFO, BASE_URI } from "../../../utils/pathMap";
+import HeaderImageScrollView from 'react-native-image-header-scroll-view';
+// teaset 
+import { Carousel } from "teaset";
+import { pxToDp } from "../../../utils/stylesKits";
+
+
 class Index extends Component {
-  state={
-    userDetail:{}
+  state = {
+    userDetail: {}
   }
-  params={
-    page:1,
-    pagesize:5
+  params = {
+    page: 1,
+    pagesize: 5
   }
 
   componentDidMount() {
-    this.getDetail();  
+    this.getDetail();
   }
 
   // 获取朋友详情
-  getDetail=async()=>{
-    const url=FRIENDS_PERSONALINFO.replace(":id",this.props.route.params.id);
-    const res=await request.privateGet(url,this.params);
-    this.setState({ userDetail:res.data  });
+  getDetail = async () => {
+    const url = FRIENDS_PERSONALINFO.replace(":id", this.props.route.params.id);
+    const res = await request.privateGet(url, this.params);
+    this.setState({ userDetail: res.data });
   }
   render() {
-    console.log(this.props.route.params);
+    // console.log(this.props.route.params);
+    const { userDetail } = this.state;
+    if (!userDetail.silder) return <></>
     return (
-      <View><Text>用户详情</Text></View>
+      <HeaderImageScrollView
+        maxHeight={pxToDp(220)}
+        minHeight={pxToDp(40)}
+        renderForeground={() => (
+          <Carousel control  style={{ height: pxToDp(220) }}>
+            {userDetail.silder.map((v, i) => <Image key={i}
+              source={{ uri: BASE_URI + v.thum_img_path }}
+              style={{ width: "100%", height: pxToDp(220) }}
+            />)}
+          </Carousel>
+        )}
+      >
+        <View >
+
+        </View>
+      </HeaderImageScrollView>
     );
   }
 }
