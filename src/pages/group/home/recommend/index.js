aa499bcf1b08a529a702a32295f8581922ad7302
uksir <yeah126139163@163.com>
2020-06-18 17:30:42 +0800

    获取推荐动态数据

--- src/pages/group/home/recommend/index.js
+++ src/pages/group/home/recommend/index.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import request from "../../../../utils/request";
+import { QZ_TJDT } from "../../../../utils/pathMap";
 class Index extends Component {
+  params={
+    page:1,
+    pagesize:10
+  }
+  state={
+    list:[]
+  }
+  componentDidMount() {
+    this.getList();  
+  }
+
+  // 获取 推荐动态的数据
+  getList=async()=>{
+    const res=await request.privateGet(QZ_TJDT,this.params);
+    console.log(res);
+    this.setState({ list: res.data });
+  }
   render() {
     return (
       <View><Text>推荐</Text></View>
