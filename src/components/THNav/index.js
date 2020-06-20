f6e2e12ef3275a5c166e70f81c640378ee3250d0
uksir <yeah126139163@163.com>
2020-06-20 14:30:37 +0800

    引入和修改公共导航栏

--- src/components/THNav/index.js
+++ src/components/THNav/index.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { View, Text, ImageBackground, StatusBar, TouchableOpacity } from 'react-native';
 import { pxToDp } from "../../utils/stylesKits";
 import IconFont from "../IconFont";
-import {NavigationContext  } from "@react-navigation/native";
+import { NavigationContext } from "@react-navigation/native";
 class Index extends Component {
-  static contextType=NavigationContext;
+  static contextType = NavigationContext;
   render() {
     // goBack
     return (
@@ -16,19 +16,22 @@ class Index extends Component {
         <ImageBackground source={require("../../res/headbg.png")}
           style={{
             height: pxToDp(60), paddingTop: pxToDp(12), flexDirection: 'row',
+            paddingLeft: pxToDp(10), paddingRight: pxToDp(10),
             alignItems: "center", justifyContent: 'space-between'
           }}
         >
           <TouchableOpacity
-          onPress={this.context.goBack}
-           style={{width:pxToDp(80), flexDirection: "row", alignItems: 'center' }}>
+            onPress={this.context.goBack}
+            style={{ width: pxToDp(80), flexDirection: "row", alignItems: 'center' }}>
             <IconFont style={{ color: "#fff" }} name="iconfanhui" />
             <Text style={{ color: "#fff" }} >返回</Text>
           </TouchableOpacity>
 
-          <Text style={{color:"#fff",fontSize:pxToDp(20),fontWeight:"bold"}}>{this.props.title}</Text>
+          <Text style={{ color: "#fff", fontSize: pxToDp(20), fontWeight: "bold" }}>{this.props.title}</Text>
 
-          <Text style={{width:pxToDp(80)}}></Text>
+          <Text
+            onPress={this.props.onRightPress || function () { }}
+            style={{ width: pxToDp(80), color: "#fff", textAlign: "right" }}>{this.props.rightText}</Text>
 
         </ImageBackground>
       </View>
--- src/pages/group/home/recommend/publish/index.js
+++ src/pages/group/home/recommend/publish/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import THNav from "../../../../../components/THNav";
 class Index extends Component {
   render() {
     return (
-      <View><Text>发布</Text></View>
+      <View>
+        <THNav title="发动态" rightText="发帖"
+          onRightPress={() => console.log("发帖了")}
+        />
+
+      </View>
     );
   }
 }
