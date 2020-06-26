cb7f9f203a6e16ed69ce56bb6eaed98f7b8191f3
uksir <yeah126139163@163.com>
2020-06-26 14:28:56 +0800

    搭建关注页面的大结构

--- src/pages/my/follow/index.js
+++ src/pages/my/follow/index.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-class Index extends Component {
-  render() {
-    return (
-      <View><Text>关注</Text></View>
-    );
-  }
+import React from 'react';
+import {
+  Text,View
+} from 'react-native';
+
+import ScrollableTabView  from 'react-native-scrollable-tab-view';
+import CustormerBar from "./components/CustormerBar";
+ import FollowEach from "./followEach";
+ import Ifollow from "./Ifollow";
+ import FollowMe from "./followMe";
+export default () => {
+  return <ScrollableTabView
+    initialPage={0}
+    renderTabBar={() => < CustormerBar/>}
+  >
+
+    <FollowEach tabLabel='互相关注'><Text>0</Text></FollowEach>
+    <Ifollow tabLabel='喜欢'><Text>1</Text></Ifollow>
+    <FollowMe tabLabel='粉丝'><Text>2</Text></FollowMe>
+ 
+  </ScrollableTabView>;
 }
-export default Index;
