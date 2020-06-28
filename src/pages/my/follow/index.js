6a34a7dc530734f410c31c053c7cce85bdeb66b4
uksir <yeah126139163@163.com>
2020-06-28 11:12:56 +0800

    渲染粉丝和互相关注页面

--- src/pages/my/follow/index.js
+++ src/pages/my/follow/index.js
@@ -1,3 +1,10 @@
+/* 
+1 互相关注-喜欢-粉丝 关系
+  1 当前登录用户 A  
+    A在用户B的详情页面中  点击了 "喜欢"  A->B
+  2 同时 在 用户B 看起来 A 就是 B 的粉丝
+  3 如果 A喜欢了B  同时 B喜欢了A  就会在 互相关注 页面中出现 
+ */
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
@@ -24,7 +31,7 @@ class index extends Component {
   // 获取喜欢相关的数据
   getList = async () => {
     const res = await request.privateGet(MY_LIKELIST);
-
+    console.log(res);
     const likeeachlist = res.data.likeeachlist;
     const ilikelist = res.data.ilikelist;
     const likemelist = res.data.likemelist;
